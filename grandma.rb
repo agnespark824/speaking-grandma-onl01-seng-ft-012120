f74a2f1ca8256287bb09f154f38b1971af8275e9
Agnes Park <agnespark824@gmail.com>
2020-01-22 08:17:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,9 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase && string != "I LOVE YOU GRANDMA!"
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   elsif string != string.upcase 
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   else 
   end
 end
